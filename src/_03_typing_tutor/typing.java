b82743ae7f17d77bf74c7a4f3c67ee44edcca413
league <league@adminisorsimac3.attlocal.net>
2025-08-23 16:28:22 -0700

    I finished from asking help..

--- src/_03_typing_tutor/typing.java
+++ src/_03_typing_tutor/typing.java
@@ -1,5 +1,8 @@
 package _03_typing_tutor;
 
+import java.awt.Color;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.Random;
 
 import javax.swing.JButton;
@@ -8,28 +11,61 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class typing {
-	public void runner(){
-	JFrame frame= new JFrame();
+public class typing implements KeyListener {
+	JLabel label = new JLabel();
 	JPanel panel = new JPanel();
-	JLabel label = new JLabel("Funny...");
-	frame.getContentPane().add(panel);
-	panel.add(label);
 	char currentLetter = 0;
-	frame.setSize(500, 500);
-	label.setFont(label.getFont().deriveFont(28.0f));
-	label.setHorizontalAlignment(JLabel.CENTER);
-	setup(frame, currentLetter);
-	}
-	void setup(JFrame frame, char currentLetter){
-		frame.setVisible(true);		
-		currentLetter = generateRandomLetter();
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		System.out.println(generateRandomLetter());
+	public void runner(){
+		JFrame frame= new JFrame();
+		
+
+		frame.getContentPane().add(panel);
+		panel.add(label);
+		frame.addKeyListener(this);
+
+		frame.setSize(500, 500);
+		label.setFont(label.getFont().deriveFont(28.0f));
+		label.setHorizontalAlignment(JLabel.CENTER);
+		setup(frame);
 
 	}
-	char generateRandomLetter() {
-	    Random r = new Random();
-	    return (char) (r.nextInt(26) + 'a');
+	void setup(JFrame frame){
+		frame.setVisible(true);		
+		currentLetter = generateRandomLetter();
+		label.setText( String.valueOf(currentLetter)  );
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
 	}
+	static char generateRandomLetter() {
+		Random r = new Random();
+		return (char) (r.nextInt(26) + 'a');
+
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		System.out.println(e.getKeyChar());
+		char letterPressed=e.getKeyChar();
+		if(letterPressed==currentLetter) {
+			panel.setBackground(Color.GREEN);
+		}else {
+			panel.setBackground(Color.RED);
+		}
+
+	}
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		currentLetter = generateRandomLetter();
+		label.setText( String.valueOf(currentLetter));
+		System.out.println(currentLetter);
+	}
+	
 }
