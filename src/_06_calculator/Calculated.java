866e37f3e1ff32525916dbd1567b76ade603286f
Aarit Jayaswal <aaritrj@gmail.com>
2025-10-04 16:31:26 -0700

    WHOVE

--- src/_06_calculator/Calculated.java
+++ src/_06_calculator/Calculated.java
@@ -2,13 +2,16 @@ package _06_calculator;
 
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.print.Printable;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class Calculated {
+public class Calculated implements ActionListener{
 	 JButton addButton;
 	 JButton subButton;
 	 JButton multiplyButton;
@@ -22,52 +25,75 @@ public class Calculated {
 	public void run() {
 		JFrame frame = new JFrame("Calculator");
 		frame.setSize(550,500);
-		JButton addButton = new JButton("add");
-		JButton subButton = new JButton("sub");
-		JButton multiplyButton = new JButton("multiply");
-		JButton divideButton = new JButton("divide");
-
+		 addButton = new JButton("add");
+		 subButton = new JButton("sub");
+		 multiplyButton = new JButton("multiply");
+		 divideButton = new JButton("divide");
 		frame.add(panel);
 		text.setColumns(20);
 		text1.setColumns(20);
 		panel.add(text);
 		panel.add(text1);
+		String s1 = text.getText();
+		System.out.println(s1);
 		panel.add(addButton);
 		panel.add(subButton);
 		panel.add(multiplyButton);
 		panel.add(divideButton);
 		panel.add(label);
+		addButton.addActionListener(this);
+		subButton.addActionListener(this);
+		multiplyButton.addActionListener(this);
+		divideButton.addActionListener(this);
+
 		frame.setVisible(true);
+		
 	}
 	 public void actionPerformed(ActionEvent e) {
 		 String s1 = text.getText();
+		 System.out.println(s1);
 		 String s2 = text1.getText();
 		 int ss1 = Integer.parseInt(s1);
 		 int ss2 = Integer.parseInt(s2);
+		 System.out.println("yup");
 		JButton buttonPressed = (JButton) e.getSource();
-		if (addButton==buttonPressed) {
+		if (buttonPressed == addButton) {
 			add(ss1,ss2);
 		}
-		 
+		if (buttonPressed == subButton) {
+			subtract(ss1,ss2);
+		}
+		if (buttonPressed == multiplyButton) {
+			multiply(ss1,ss2);
+		}
+		if (buttonPressed == divideButton) {
+			divide(ss1,ss2);
+		}
 	 }
 	
 	 void add(int ss1, int ss2){
 	 int adds=ss1+ss2;
 	 label.setText(String.valueOf(adds));
+	 System.out.println(adds);
 	}
 	 
-	 void subtract() {
-	 	 
-	 	 
+	 void subtract(int ss1, int ss2) {
+	 	 int minus=ss1-ss2;
+	 	 label.setText(String.valueOf(minus));
+	 	 System.out.println(minus);
 	 }
 	 
-	 void multiply() {
-	 
+	 void multiply(int ss1, int ss2) {
+	 	 int times=ss1*ss2;
+	 	 label.setText(String.valueOf(times));
+	 	 System.out.println(times);
 	 
 	 }
 	 
-	 void divide() {
-		 
+	 void divide(int ss1, int ss2) {
+	 	 int over=ss1/ss2;
+	 	 label.setText(String.valueOf(over));
+	 	 System.out.println(over);
 		 
 	 }
 }
