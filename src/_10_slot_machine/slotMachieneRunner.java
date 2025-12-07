82100323853569bbeb78b893499e38e27d8b660c
league <maddoxseul@sbsd.net>
2025-12-06 16:29:52 -0800

    I did some of this gambling game

---
+++ src/_10_slot_machine/slotMachieneRunner.java
@@ -0,0 +1,11 @@
+package _10_slot_machine;
+
+public class slotMachieneRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			new slotMachine().run();
+			slotMachine jk = new slotMachine();
+	}
+
+}
--- src/_10_slot_machine/slotMachine.java
+++ src/_10_slot_machine/slotMachine.java
@@ -1,19 +1,38 @@
 package _10_slot_machine;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.net.URL;
 
+import javax.imageio.ImageIO;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-
-public class slotMachine {
-	JFrame frame = new JFrame("Machine");
-	JPanel panel = new JPanel();
-	public void run() {
+import javax.swing.JButton;
+public class slotMachine implements MouseListener, ActionListener {
+//	 orange = ImageIO.read(getClass().getResource("orange.png"));
+    JFrame frame = new JFrame();
+    JPanel panel = new JPanel();
+    JLabel label;
+    JLabel label2;
+    JLabel label3;
+    JButton button = new JButton();
+	public void run() {	
 		frame.add(panel);
-		orange = createLabelImage("orange.png");
+		panel.add(button);
+		button.addActionListener(this);
+		label = createLabelImage("orange.png");
+		panel.add(label);
+		label2 = createLabelImage("cherry.png");
+		panel.add(label2);
+		label3 = createLabelImage("banana.png");
+		panel.add(label3);
+		frame.pack();
+		frame.setVisible(true);
 
 	}
 
@@ -27,5 +46,44 @@ private JLabel createLabelImage(String fileName) {
 		imageLabel.setIcon(icon);
 	}
 	return imageLabel;
+}
+
+@Override
+public void mouseClicked(MouseEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+
+public void mousePressed(MouseEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void mouseReleased(MouseEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void mouseEntered(MouseEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void mouseExited(MouseEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void actionPerformed(ActionEvent e) {
+	// TODO Auto-generated method stub
+	panel.remove(label);
+	panel.remove(label2);
+	panel.remove(label3);
+	// TODO: use random numbers to switch through diffrnent values you could see
+	panel.repaint();
 }
 }
